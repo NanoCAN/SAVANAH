e59db1ebd50d7f099e8a6a8c0b9b7f405f2524d3
mlist <mlist@mpi-inf.mpg.de>
2016-03-03 11:33:50 +0100

    added HtsBackend as git submodule

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -7,8 +7,7 @@ grails.project.source.level = 1.6
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 grails.project.dependency.resolver = "maven"
 
-//grails.plugin.location.Rconnect = "../Rconnect/"
-grails.plugin.location.HtsBackend = "../../HtsBackend"
+grails.plugin.location.HtsBackend = "HtsBackend"
 
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
